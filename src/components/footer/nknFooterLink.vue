e6ae660028a201134d754daafc14ed46f6d81527
heqian <664516715@qq.com>
2018-12-19 18:08:01 +0800

    add <a> to footerlink & remove useless plugins

--- src/components/footer/nknFooterLink.vue
+++ src/components/footer/nknFooterLink.vue
@@ -1,86 +1,116 @@
 <template>
-  <div class="footer-link clearfix">
-    <div class="footer-link-module">
-      <ul>
-        <li class="footer-link-module-title">Product</li>
-        <li class="footer-link-module-tab">
-          <i class="icon iconfont icon-developer"></i>
-          <span>Developer</span>
-        </li>
-        <li class="footer-link-module-tab">
-          <i class="icon iconfont icon-miner"></i>
-          <span>Miner</span>
-        </li>
-        <li class="footer-link-module-tab">
+<div class="footer-link clearfix">
+  <div class="footer-link-module">
+    <ul>
+      <li class="footer-link-module-title">Product</li>
+      <li class="footer-link-module-tab">
+        <a href="#">
+            <i class="icon iconfont icon-developer"></i>
+            <span>Developer</span>
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
+            <i class="icon iconfont icon-miner"></i>
+            <span>Miner</span>
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-partner"></i>
           <span>Partner</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-community"></i>
           <span>Community</span>
-        </li>
-      </ul>
-    </div>
-    <div class="footer-link-module">
-      <ul>
-        <li class="footer-link-module-title">Community</li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-link-module">
+    <ul>
+      <li class="footer-link-module-title">Community</li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-telegram"></i>
           <span>Telegram</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-twitter"></i>
           <span>Twitter</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-reddit"></i>
           <span>Reddit</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-discord"></i>
           <span>Discord</span>
-        </li>
-      </ul>
-    </div>
-    <div class="footer-link-module">
-      <ul>
-        <li class="footer-link-module-title">Resource</li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-link-module">
+    <ul>
+      <li class="footer-link-module-title">Resource</li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-github"></i>
           <span>Github</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-word"></i>
           <span>Introduction</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-word"></i>
           <span>Whitepaper</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-download"></i>
           <span>Logo Download</span>
-        </li>
-      </ul>
-    </div>
-    <div class="footer-link-module">
-      <ul>
-        <li class="footer-link-module-title">Focus</li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+    </ul>
+  </div>
+  <div class="footer-link-module">
+    <ul>
+      <li class="footer-link-module-title">Focus</li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-joinus"></i>
           <span>Join us</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-news"></i>
           <span>News</span>
-        </li>
-        <li class="footer-link-module-tab">
+          </a>
+      </li>
+      <li class="footer-link-module-tab">
+        <a href="#">
           <i class="icon iconfont icon-blog"></i>
           <span>Blog</span>
-        </li>
-      </ul>
-    </div>
+          </a>
+      </li>
+    </ul>
   </div>
+</div>
 </template>
 
 <script>
@@ -90,31 +120,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.footer-link{
+.footer-link {
   padding: 90px 0;
-  &-module{
+
+  &-module {
     float: left;
     min-width: 300px;
     color: #fff;
 
-    &-title{
-      font-size: 24px;color: currentColor;
+    &-title {
+      font-size: 24px;
+      color: currentColor;
       padding: 25px 0;
     }
 
-    &-tab{
-      font-size: 18px;color: currentColor;
+    &-tab {
+      font-size: 18px;
+      color: currentColor;
       padding: 15px 0;
       cursor: pointer;
 
-      &>i{
-        margin-right: 16px;
-        font-size: 24px;
-        vertical-align: -1px;
+      &>a {
+        color: currentColor;
+
+        &>i {
+          margin-right: 16px;
+          font-size: 24px;
+          vertical-align: -1px;
+        }
       }
 
       &:hover {
-        color:#1791F2;
+        color: #1791F2;
       }
     }
   }
